acb260cbcf8b55b8127efe0c6be9792fad70cd01
SHEHEROZ1FF <ayansharoz424@gmail.com>
2024-09-22 21:59:16 +0600

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ require("./bot.js");
 ║                                                                        ║
 ║  ## Created by GlaceYT!                                                ║
 ║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
+║  ## DISCORD :  https://discord.gg/wvq9AuCeEr.                          ║
+║  ## YouTube : https://youtube.com/@sheheroz1ff?si=zrFTwBKLm8qGtemx     ║
 ║                                                                        ║
 ╚════════════════════════════════════════════════════════════════════════╝
 
